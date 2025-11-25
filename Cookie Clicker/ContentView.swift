4e6e46b3e19eee738678f2ea81d1405c882fab12
Joseph <jjphillipse4@gmail.com>
2025-11-25 10:37:35 -0600

    1st didn't stage the first

--- Cookie Clicker/ContentView.swift
+++ Cookie Clicker/ContentView.swift
@@ -2,20 +2,35 @@
 //  ContentView.swift
 //  Cookie Clicker
 //
-//  Created by joseph phillips on 11/25/25.
+//  Created by joseph phillips on 11/20/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Image("cookie")
+                .resizable()
+                .frame(width: 100, height: 100)
+            
+            Text("Score: \(score)")
+                .font(.largeTitle)
+                .padding()
+            
+            Spacer()
+            
+            Button("Tap Me!") {
+                score += 1
+            }
+            .font(.title)
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
         }
-        .padding()
     }
 }
 
