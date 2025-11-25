83dd1df985ce13b6e97c7ed6ad71803a84abbc2c
Joseph <jjphillipse4@gmail.com>
2025-11-25 10:25:05 -0600

    Initial Commit

---
+++ Cookie Clicker/Cookie_ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cookie_ClickerApp.swift
+//  Cookie Clicker
+//
+//  Created by joseph phillips on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Cookie_ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
